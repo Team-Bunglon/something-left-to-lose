40327192e83bf7eda8c2ff9718f03d8743e9a3c1
KenKomKom <kenichikomala@gmail.com>
2024-04-10 19:23:03 +0700

    implemented sistem sandi

---
+++ scenes/vending_machine.gd
@@ -0,0 +1,19 @@
+## INI HARUSNYA PUNYA LOKER
+
+extends transparancable_object
+
+onready var command_line = $LineEdit
+var _sandi = ""
+
+func _ready():
+	command_line.visible=false
+
+func set_sandi(sandi):
+	_sandi=sandi
+
+func interact():
+	command_line.visible=true
+
+func _on_LineEdit_text_entered(new_text):
+	if new_text.to_lower()==_sandi.to_lower():
+		print("berhasil")
---
+++ scripts/Level1.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var sandi_locker = ""
+var rng = RandomNumberGenerator.new()
+onready var kertas_sandi = $kertassandi
+
+func _ready():
+	sandi_locker = _generate_sandi()
+	
+	###
+	# Ini masih perlu ganti instance tujuan harusnya
+	kertas_sandi.set_sandi(sandi_locker)
+	$tembok2/vending_machine.set_sandi(sandi_locker)
+	###
+
+func _generate_sandi():
+	var sandi = ""
+	for i in range(0,5):
+		var char_type_decision = rng.randi()%2
+		var alphabet = rng.randi_range(48,57)
+		if char_type_decision==1:
+			alphabet = rng.randi_range(65,90)
+		sandi+=char(alphabet)
+	return sandi
