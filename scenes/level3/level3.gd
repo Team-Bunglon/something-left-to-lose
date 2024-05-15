a819f916bccf1815df19e2b23fbcbc9537ab45a7
navisraditya <navisraditya@gmail.com>
2024-05-15 10:35:47 +0700

    init???.

--- scenes/level3/level3.gd
+++ scenes/level3/level3.gd
@@ -12,18 +12,18 @@ var current_dialogue_index = 0
 
 func _ready():
 	print(PLAYER_STATES.stamina)
-#	if Level3Manager.first_time:
-#		if dialogues.size() > 0:
-#			DialogueBoxManager.emit_signal("type", dialogues[current_dialogue_index])
+	if Level3Manager.first_time:
+		if dialogues.size() > 0:
+			DialogueBoxManager.emit_signal("type", dialogues[current_dialogue_index])
 
 func _process(delta):
 	if PLAYER_STATES.stamina <= 0:
 		Level3Manager.first_time = false
 		PLAYER_STATES.decrease_stamina(10)
 		get_tree().change_scene("res://scenes/level3/level3.tscn")
-#	if Level3Manager.first_time and Input.is_action_pressed("ui_accept"):
-#		if current_dialogue_index < dialogues.size() - 1:
-#			current_dialogue_index += 1
-#			DialogueBoxManager.emit_signal("type", dialogues[current_dialogue_index])
+	if Level3Manager.first_time and Input.is_action_pressed("ui_accept"):
+		if current_dialogue_index < dialogues.size() - 1:
+			current_dialogue_index += 1
+			DialogueBoxManager.emit_signal("type", dialogues[current_dialogue_index])
 		
 
