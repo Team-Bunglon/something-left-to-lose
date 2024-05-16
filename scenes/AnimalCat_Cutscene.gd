18bb4aa1714cfd362dcbae7138e2d7759b33dcfe
Farras Hafizhudin <farras.hafizhudin@ui.ac.id>
2024-05-16 08:38:11 +0700

    added new content & dialogs, reimplement cat approach & enemy entity

--- scenes/AnimalCat_Cutscene.gd
+++ scenes/AnimalCat_Cutscene.gd
@@ -4,7 +4,6 @@ func _ready():
 	pass
 
 func _process(delta):
-	$AnimatedSprite.play("running-right")
 	var target_position = Vector2(1320, -230)
 	var move_speed = 50
 	var current_position = position
