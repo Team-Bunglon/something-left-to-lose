343229cd6b956e9a0bdf75b0d751e9713b8b81b3
KenKomKom <kenichikomala@gmail.com>
2024-05-20 21:05:59 +0700

    perubahan di mejacpu dan level1 ganti pintu

--- scenes/glow.gd
+++ scenes/glow.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 func _ready():
-	PLAYER_STATES.connect("change_state",self,"glow_parent")
+#	PLAYER_STATES.connect("change_state",self,"glow_parent")
+	pass
 
 func glow_parent(state):
-	if state==PLAYER_STATES.STATES.SMART:
-		self.get_parent().glow()
-	else:
-		self.get_parent().glow()
+	pass
+#	if state==PLAYER_STATES.STATES.SMART:
+#		self.get_parent().glow()
+#	else:
+#		self.get_parent().glow()
