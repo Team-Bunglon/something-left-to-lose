f96136c4d61370bc93437096dd7a3e28d6d3175f
Farras Hafizhudin <farras.hafizhudin@ui.ac.id>
2024-05-20 16:35:15 +0700

    polished deathscene

--- scenes/CharDeathAnim.gd
+++ scenes/CharDeathAnim.gd
@@ -2,32 +2,44 @@ extends Node2D
 
 var idle_timer := Timer.new()
 var blood_timer := Timer.new()
+var restart_menu_timer := Timer.new()
+onready var restart_button = get_node("TransitionScreen1/Control/RestartButton")
 
 func _ready():
 	add_child(idle_timer)
 	add_child(blood_timer)
+	add_child(restart_menu_timer)
 	
 	idle_timer.connect("timeout", self, "_on_idle_timer_timeout")
 	blood_timer.connect("timeout", self, "_on_blood_timer_timeout")
+	restart_menu_timer.connect("timeout", self, "_on_restart_menu_timer_timeout")
 
 	idle_timer.wait_time = 1.7
 	idle_timer.one_shot = true
 	idle_timer.start()
 
-	$AnimationPlayer.play("death-anim")
 	$Char.visible = true
-	$BloodSplat.visible = false
+	$AnimationPlayer.play("death-anim")
 
 func _on_idle_timer_timeout():
 	$Char.visible = false
 	$BloodSplat.visible = true
 	
-	blood_timer.wait_time = 1
+	blood_timer.wait_time = 0.5
 	blood_timer.one_shot = true
 	blood_timer.start()
 
 	$AnimationPlayer.play("blood-splat")
 
 func _on_blood_timer_timeout():
-	var TransitionScreen1 = get_node("TransitionScreen1")
-	TransitionScreen1.change_scene("res://scenes/Level2Lanjutan.tscn")
+	$BloodSplat.visible = false
+	$BloodOverlay.visible = true
+	
+	restart_menu_timer.wait_time = 0.6
+	restart_menu_timer.one_shot = true
+	restart_menu_timer.start()
+	
+	$AnimationPlayer.play("blood-overlay")
+	
+func _on_restart_menu_timer_timeout():
+	restart_button.visible = true
