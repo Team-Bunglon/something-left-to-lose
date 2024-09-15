2e4632638561aa616fe84e443a6bb6437fc9cfa9
figoathallah <figoirawan02@gmail.com>
2024-09-15 16:48:27 +0700

    main menu design revamp, functionality wip

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -5,12 +5,12 @@ export (String) var next_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$PlayButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
 
 
 func _on_PlayButton_pressed():
