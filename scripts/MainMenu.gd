8da64fd8e18e88362022dbf0e0567a513de44a17
figoathallah <figoirawan02@gmail.com>
2024-09-16 20:36:29 +0700

    add select SFX on main menu buttons

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -5,7 +5,13 @@ export (String) var next_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var buttons = get_tree().get_nodes_in_group("button")
 	$PlayButton.grab_focus()
+				
+	for button in buttons:
+		button.connect("mouse_entered", self, "_on_button_entered")
+		button.connect("focus_entered", self, "_on_focus_entered")
+			
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +26,10 @@ func _on_PlayButton_pressed():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+	
+func _on_button_entered():
+	$SelectSFX.play()
+	
+
+func _on_focus_entered():
+	$SelectSFX.play()
