57bb0854b59c83e4e94af11ad2c8e0ff843e3f7a
figoathallah <figoirawan02@gmail.com>
2024-09-22 16:38:44 +0700

    add pause menu functionality

---
+++ scenes/Scenery/pausemenu.gd
@@ -0,0 +1,45 @@
+extends CanvasLayer
+
+export (String) var next_scene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var buttons = get_tree().get_nodes_in_group("button")
+				
+	for button in buttons:
+		button.connect("mouse_entered", self, "_on_button_entered")
+		button.connect("focus_entered", self, "_on_focus_entered")
+			
+	$Options.pause_mode = Node.PAUSE_MODE_PROCESS
+	$SelectSFX.pause_mode = Node.PAUSE_MODE_PROCESS
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _on_button_entered():
+	$SelectSFX.play()
+
+func _on_button_focused():
+	$SelectSFX.play()
+
+func _on_PlayButton_pressed():
+	self.visible = false
+	get_tree().paused = false
+
+
+func _on_OptionsButton_pressed():
+	$Control.visible = false
+	$Options.visible = true
+	
+	
+	
+func _on_Options_closedMenu():
+	$Control.visible = true
+
+
+func _on_QuitButton_pressed():
+	get_tree().paused = false
+	$TransitionScreen1.visible = true
+	$TransitionScreen1.change_scene(next_scene)
+
--- scripts/Level1.gd
+++ scripts/Level1.gd
@@ -6,11 +6,14 @@ onready var kertas_sandi = $kertassandi
 
 onready var level1BGM = $LightHumBGM
 
+onready var pauseMenu = $PauseMenu
+
 func _ready():
 	PLAYER_STATES.keySFX = $KeySFX
 	PLAYER_STATES.paperSFX = $PaperSFX
 	level1BGM.play()
 	level1BGM.pause_mode = Node.PAUSE_MODE_PROCESS
+	pauseMenu.pause_mode = Node.PAUSE_MODE_PROCESS
 	sandi_locker = _generate_sandi()
 	###
 	# Ini masih perlu ganti instance tujuan harusnya
@@ -20,6 +23,11 @@ func _ready():
 	
 	DialogueBoxManager.emit_signal("type", """The door is locked.
 	You don't know if the key even exists.""")
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_pause"):
+		get_tree().paused = true
+		$PauseMenu.visible = true
 
 func _generate_sandi():
 	var sandi = ""
@@ -30,3 +38,10 @@ func _generate_sandi():
 			alphabet = rng.randi_range(65,90)
 		sandi+=char(alphabet)
 	return sandi
+		
+		
+		
+		
+		
+		
+		
