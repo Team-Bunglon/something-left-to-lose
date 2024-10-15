cbb0ec9fe053e30385f36da4aaa1f633e9631a00
figoathallah <figoirawan02@gmail.com>
2024-10-15 15:51:38 +0700

    preliminary mini level dialogue

--- scripts/mini-level.gd
+++ scripts/mini-level.gd
@@ -1,9 +1,22 @@
 extends Node2D
 
 onready var player_cam = get_node("player/Camera2D")
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+
+var dialogues = [
+	"[Raka]\nSomehow we ended up here... in this park.",
+	"[Strong Raka]\nWOO!! I love parks. I didn't know we had one this huge within this campus!",
+	"[Smart Raka]\nPipe down, you dorks. We'll lose sight of the cat.",
+	"[Strong Raka]\nHey hey, lighten up! We've been able to keep up so far. This is a piece of cake!"
+]
+
+var hedge_encounter_dialogues = [
+	"[Strong Raka]\n...",
+	"[Raka]\nUhh...",
+	"[Smart Raka]\nYou were saying?",
+	"[Strong Raka]\n...Do we really have to go through this?"
+]
+
 
 
 # Called when the node enters the scene tree for the first time.
