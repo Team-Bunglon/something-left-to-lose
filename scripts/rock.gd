f914d266590a784d2ab97c351077664962af0ebf
figoathallah <figoirawan02@gmail.com>
2024-11-05 00:38:48 +0700

    add comprehensive sledgehammer usage for breaking rocks in level 4

--- scripts/rock.gd
+++ scripts/rock.gd
@@ -7,11 +7,13 @@ signal break_rock
 
 func _process(delta):
 	if in_area and Input.is_action_just_pressed("ui_accept"):
-		if player and player.current_state == PLAYER_STATES.STATES.STRONG:
+		if player and player.current_state == PLAYER_STATES.STATES.STRONG and Level4Manager.has_sledgehammer:
 			emit_signal("break_rock")
-			self.queue_free()
+			self.queue_free()		
+		elif player and player.current_state != PLAYER_STATES.STATES.STRONG and Level4Manager.has_sledgehammer:
+			DialogueBoxManager.emit_signal("type", "[Strong Raka]\nSwitch to me so I can break these rocks with ease with the sledgehammer!")
 		else:
-			DialogueBoxManager.emit_signal("type", "[Strong Raka]\nSwitch to me so I can break these rocks with ease!")
+			DialogueBoxManager.emit_signal("type", "[Raka]\nI need to find something to break these rocks with...")
 	
 func _on_RockObstacle_body_entered(body):
 	if body.name == "player":
